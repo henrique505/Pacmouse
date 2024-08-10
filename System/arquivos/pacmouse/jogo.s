3395dae51c390a3249fbda17fbf5247b1b99d6ce
henrique505 <henriquedeeealmeida@gmail.com>
2024-08-10 14:26:40 -0300

    Update jogo.s

--- System/arquivos/pacmouse/jogo.s
+++ System/arquivos/pacmouse/jogo.s
@@ -1,4 +1,9 @@
 .data
+
+.include "./arquivos .data/levels/tile.data"
+.include "./arquivos .data/levels/fundo1.data"
+.include "./arquivos .data/char/char.data"
+
 CHAR_POS:	.half 0,0			# x, y
 OLD_CHAR_POS:	.half 0,0			# x, y
 
@@ -17,12 +22,17 @@ GAME_LOOP:	call KEY2			# chama o procedimento de entrada do teclado
 		xori s0,s0,1			# inverte o valor frame atual (somente o registrador)
 		
 		la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
-		
+
 		la a0,char			# carrega o endereco do sprite 'char' em a0
 		lh a1,0(t0)			# carrega a posicao x do personagem em a1
 		lh a2,2(t0)			# carrega a posicao y do personagem em a2
 		mv a3,s0			# carrega o valor do frame em a3
 		call PRINT			# imprime o sprite
+		
+		la t0,OLD_CHAR_POS
+		lh a1,0(t0)			# carrega a posicao x do personagem em a1
+		lh a2,2(t0)			# carrega a posicao y do personagem em a2
+		call ERASE
 		
 		li t0,0xFF200604		# carrega em t0 o endereco de troca de frame
 		sw s0,0(t0)			# mostra o sprite pronto para o usuario
@@ -30,15 +40,18 @@ GAME_LOOP:	call KEY2			# chama o procedimento de entrada do teclado
 		#####################################
 		# Limpeza do "rastro" do personagem #
 		#####################################
-		la t0,OLD_CHAR_POS		# carrega em t0 o endereco de OLD_CHAR_POS
 		
-		la a0,tile			# carrega o endereco do sprite 'tile' em a0
-		lh a1,0(t0)			# carrega a posicao x antiga do personagem em a1
-		lh a2,2(t0)			# carrega a posicao y antiga do personagem em a2
 		
-		mv a3,s0			# carrega o frame atual (que esta na tela em a3)
-		xori a3,a3,1			# inverte a3 (0 vira 1, 1 vira 0)
-		call PRINT			# imprime
+		
+		#la t0,OLD_CHAR_POS		# carrega em t0 o endereco de OLD_CHAR_POS
+		
+		#la a0,tile			# carrega o endereco do sprite 'tile' em a0
+		#lh a1,0(t0)			# carrega a posicao x antiga do personagem em a1
+		#lh a2,2(t0)			# carrega a posicao y antiga do personagem em a2
+		
+		#mv a3,s0			# carrega o frame atual (que esta na tela em a3)
+		#xori a3,a3,1			# inverte a3 (0 vira 1, 1 vira 0)
+		#call PRINT			# imprime
 
 		j GAME_LOOP			# continua o loop
 
@@ -46,6 +59,7 @@ KEY2:		li t1,0xFF200000		# carrega o endereco de controle do KDMMIO
 		lw t0,0(t1)			# Le bit de Controle Teclado
 		andi t0,t0,0x0001		# mascara o bit menos significativo
    		beq t0,zero,FIM   	   	# Se nao ha tecla pressionada entao vai para FIM
+  		
   		lw t2,4(t1)  			# le o valor da tecla tecla
 		
 		li t0,'w'
@@ -107,7 +121,7 @@ CHAR_BAIXO:	la t0,CHAR_POS			# carrega em t0 o endereco de CHAR_POS
 		
 
 #################################################
-#	a0 = endereço imagem			#
+#	a0 = endereÃ§o imagem			#
 #	a1 = x					#
 #	a2 = y					#
 #	a3 = frame (0 ou 1)			#
@@ -149,7 +163,7 @@ PRINT_LINHA:	lw t6,0(t1)			# carrega em t6 uma word (4 pixeis) da imagem
 
 		addi t0,t0,320			# t0 += 320
 		sub t0,t0,t4			# t0 -= largura da imagem
-		# ^ isso serve pra "pular" de linha no bitmap display
+		#  isso serve pra "pular" de linha no bitmap display
 		
 		mv t3,zero			# zera t3 (contador de coluna)
 		addi t2,t2,1			# incrementa contador de linha
@@ -157,8 +171,46 @@ PRINT_LINHA:	lw t6,0(t1)			# carrega em t6 uma word (4 pixeis) da imagem
 		
 		ret				# retorna
 
-.data
-.include "./arquivos/pacmouse/arquivos .data/levels/tile.data"
-.include "./arquivos/pacmouse/arquivos .data/levels/fundo1.data"
-.include "./arquivos/pacmouse/arquivos .data/char/char.data"
+ERASE:		# a1 = x, a2 = y
+		li t0,0xFF0			# carrega 0xFF0 em t0
+		add t0,t0,a3			# adiciona o frame ao FF0 (se o frame for 1 vira FF1, se for 0 fica FF0)
+		slli t0,t0,20			# shift de 20 bits pra esquerda (0xFF0 vira 0xFF000000, 0xFF1 vira 0xFF100000)
+		
+		# adiciona x ao t0
+		li t1,320			# t1 = 320
+		mul t1,t1,a2			# t1 = 320 * y
+		add t1,t1,a1 			# t1 = (320 * y) + x / adiciona x ao t1
+		add t0,t0,t1			# adiciona t1 ao t0
+		# t0 agora é nosso endereço do ponto na tela
+		
+		la t2, fundo1
+		addi t2, t2, 8
+		# t0 é o nosso endereço, t2 é o ponteiro do fundo
+		add t2, t2, t1
+		
+		li s1, 0 # contador de Y
+		li s2, 0 # contador de X
+		li s3, 320
+		
+		LOOP_Y:
+		 li s2, 0                   # contador de X (colunas)
+			LOOP_X:
+				 lw t3, 0(t2)               # carrega a palavra de 4 pixels do fundo em t3
+  				 sw t3, 0(t0)               # escreve os 4 pixels no bitmap na posição antiga
+    				 addi t2, t2, 4             # incrementa o endereco da imagem (fundo)
+    				 addi t0, t0, 4             # incrementa o endereco do bitmap (posição antiga)
+    				 addi s2, s2, 4             # incrementa o contador de colunas
+        			 blt s2, s3, LOOP_X         # se nao alcancou o limite de pixels, repete o loop de colunas
+				
+			LOOP_X_END: 
+			   addi t0, t0, 320           # move o endereço do bitmap para a próxima linha (320 bytes)
+  			  addi t2, t2, 320           # move o ponteiro do fundo para a próxima linha
+    			addi s1, s1, 1             # incrementa o contador de linhas
+    			blt s1, s3, LOOP_Y         # repete o loop de linhas até o limite (16 pixels)
+		LOOP_Y_END: 
+		ret 				#retorna
+		
+		
+		
+		
 
