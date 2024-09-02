d2905bd26629ab31a36a9ab2f804d6f73f248656
NyveaPaulla <241040207@aluno.unb.br>
2024-09-02 20:37:07 -0300

    Add files via upload

---
+++ System/arquivos/pacmouse/music/gameover.s
@@ -0,0 +1,30 @@
+.data
+# Numero de Notas a tocar
+GAMEOVER: .word 10
+# lista de nota, duracao, nota, duracao...
+NOTAS: 65, 214, 69, 214, 65, 214, 63, 214, 62, 856
+
+.text
+	la s0,GAMEOVER		# define o endereço do número de notas
+	lw s1,0(s0)		# le o numero de notas
+	la s0,NOTAS		# define o endereço das notas
+	li t0,0			# zera o contador de notas
+	li a2,34		# define o instrumento
+	li a3,127		# define o volume
+
+LOOP:	beq t0,s1, FIM		# contador chegou no final? então  vá para FIM
+	lw a0,0(s0)		# le o valor da nota
+	lw a1,4(s0)		# le a duracao da nota
+	li a7,31		# define a chamada de syscall
+	ecall			# toca a nota
+	mv a0,a1		# passa a duração da nota para a pausa
+	li a7,32		# define a chamada de syscal 
+	ecall			# realiza uma pausa de a0 ms
+	addi s0,s0,8		# incrementa para o endereço da próxima nota
+	addi t0,t0,1		# incrementa o contador de notas
+	j LOOP			# volta ao loop
+	
+FIM:
+	li a7,10		# define o syscall Exit
+	ecall			# exit
+
